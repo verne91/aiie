a347263869589e91fc63edef29bc5d7a22293440
Chen <verne91@gmail.com>
2018-06-27 11:29:58 -0400

    add read cutoff

--- run_aiie.py
+++ run_aiie.py
@@ -17,15 +17,16 @@ if __name__ == "__main__":
 
     repeat_parser = parser.add_argument_group("Repeat files")
     repeat_parser.add_argument('--aluSeq', required=True, metavar='ALU_SEQ', dest='alu_seq_file', help='Alu sequence fasta file')
-    repeat_parser.add_argument('--allRepeatSeq',required=True, metavar='ALL_REPEAT_SEQ', dest='All_repeat_file', help='All repeat sequence fasta file')
+    repeat_parser.add_argument('--allRepeatSeq',required=True, metavar='ALL_REPEAT_SEQ', dest='all_repeat_file', help='All repeat sequence fasta file')
     repeat_parser.add_argument('--aluCoord', required=True, metavar='ALU_COORD', dest='alu_coord_file', help='ALU coordinates in genome (3-column bed file)')
     repeat_parser.add_argument('--simpleRepeatCoord', required=True, metavar='SIMPLE_REPEAT_COORD', dest='simple_repeat_coord_file', help='simple repeat coordinates in genome (3-column bed file)')
 
     cutoff_parser = parser.add_argument_group("Cutoff parameters")
-    cutoff_parser.add_argument('--aluKmer', default=13, type=int, metavar='ALU_KMER', dest='alu_k', help='k-mer of Alu side (defalut: 13)')
-    cutoff_parser.add_argument('--refKmer', default=13, type=int, metavar='REF_KMER', dest='ref_k', help='k-mer of reference side (defalut: 13)')
-    cutoff_parser.add_argument('--extendLen', default=10, type=int, metavar='EXTENDED_LENGTH', dest='extend_len', help='extended length to check the match (defalut: 10)')
-    cutoff_parser.add_argument('--allowMis', default=2, type=int, metavar='ALLOW_MISMATCH', dest='allow_mis', help='maximum allowing mismatch base pairs (defalut: 2)')
+    cutoff_parser.add_argument('--aluKmer', default=13, type=int, metavar='ALU_KMER', dest='alu_k', help='k-mer of Alu side (default: 13)')
+    cutoff_parser.add_argument('--refKmer', default=13, type=int, metavar='REF_KMER', dest='ref_k', help='k-mer of reference side (default: 13)')
+    cutoff_parser.add_argument('--extendLen', default=10, type=int, metavar='EXTENDED_LENGTH', dest='extend_len', help='extended length to check the match (default: 10)')
+    cutoff_parser.add_argument('--allowMis', default=2, type=int, metavar='ALLOW_MISMATCH', dest='allow_mis', help='maximum allowing mismatch base pairs (default: 2)')
+    cutoff_parser.add_argument('--minSupportRead', default=2, type=int, metavar='MIN_SUPPORT_READ', dest='min_support_read', help="minimum supported reads (default: 2")
 
     output_parser = parser.add_argument_group("Output options")
     output_parser.add_argument('-o', '--out', required=True, metavar='OUT', dest='out_file', help='output file')
